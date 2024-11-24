368128aa2f4bc3c060718b5f209b2d179f8f4ceb
Manish Reddy <89566773+Manish001234@users.noreply.github.com>
2024-11-24 16:05:23 +0530

    navfix2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  
 
   const handleMenuToggle = () => {
     setIsMenuOpen(!isMenuOpen);
